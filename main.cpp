7a045d182cbbbbe731aee54a752160e8af2577b2
Ricardo Alves <ralves@salientpartners.com>
2015-06-30 17:19:03 -0500

    change under branch

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main( int argc, char* argv{} )
 
    std::cout << "Hello world!" << std::endl;
    std::cout << "Just added another line" << std::endl;
+   std::cout << "added line in branch" << std::endl;
    return 0;
 }
