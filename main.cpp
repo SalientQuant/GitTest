a0f910ca78ae838b8e64101c25c29e8f98f1a743
Ricardo Alves <ralves@salientpartners.com>
2015-07-02 11:21:38 -0500

    one more line added to the file

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main( int argc, char* argv{} )
    std::cout << "Hello world!" << std::endl;
    std::cout << "Just added another line" << std::endl;
    std::cout << "added line in branch" << std::endl;
+   std::cout << "Line to be reviewed by Xuan" << std::endl;
    return 0;
 }
